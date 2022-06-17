8cdc32a2a632c87d7c3758d7bf57dbf10944f046
Sergey <ser_8904@mail.ru>
2022-06-17 10:54:59 +0300

    add static pages & tests

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,8 @@
 Rails.application.routes.draw do
   get 'static_pages/home'
   get 'static_pages/help'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  get 'static_pages/about'
 
-  # Defines the root path route ("/")
   root 'home#index'
+
 end

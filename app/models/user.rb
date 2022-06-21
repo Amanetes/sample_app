a3217e48f98a18a46e69bf4e1bd7a7150eb9cd5d
Sergey <ser_8904@mail.ru>
2022-06-21 21:45:58 +0300

    Finish user signup

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@
 
 class User < ApplicationRecord
   before_save { email.downcase! }
+  # before_validation { self.email = email.downcase }
 
   validates :name, presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d-]+(\.[a-z\d-]+)*\.[a-z]+\z/i

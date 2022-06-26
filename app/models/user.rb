529d3954c2fafc7b5e0e9a87764a12a29cbd9426
Sergey <ser_8904@mail.ru>
2022-06-26 10:23:50 +0300

    Add password reset

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
-  attr_accessor :remember_token, :activation_token
+  attr_accessor :remember_token, :activation_token, :reset_token
 
   before_save :downcase_email # включает 2 действия create & update
   before_create :create_activation_digest # включает 1 действие только create
@@ -22,7 +22,7 @@ class User < ApplicationRecord
     BCrypt::Password.create(string, cost:)
   end
 
-  # Returns a random token. Необходимо для "запоминания сессии"
+  # Returns a random token.
   def self.new_token
     SecureRandom.urlsafe_base64
   end
@@ -63,6 +63,22 @@ class User < ApplicationRecord
   # Sends activation email
   def send_activation_email
     UserMailer.account_activation(self).deliver_now
+  end
+
+  # Sets the password reset attributes.
+  def create_reset_digest
+    self.reset_token = User.new_token
+    update_columns(reset_digest: User.digest(reset_token), reset_sent_at: Time.zone.now)
+  end
+
+  # Sends password reset email. .password_reset is a user_mailer method
+  def send_password_reset_email
+    UserMailer.password_reset(self).deliver_now
+  end
+
+  # Returns true if a password reset has expired.
+  def password_reset_expired?
+    reset_sent_at < 2.hours.ago
   end
 
   private

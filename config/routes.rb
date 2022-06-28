8f0df60d92c432c276f614c9e839fda4521f6d59
Sergey <ser_8904@mail.ru>
2022-06-28 16:33:21 +0300

    Add user following

--- config/routes.rb
+++ config/routes.rb
@@ -13,9 +13,14 @@ Rails.application.routes.draw do
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
 
-  resources :users
+  resources :users do
+    member do # member отвечает на url с id /user/1/followers
+      get :following, :followers
+    end
+  end
   resources :account_activations, only: [:edit]
   resources :password_resets, only: %i[new create edit update]
   resources :microposts, only: %i[create destroy]
+  resources :relationships, only: %i[create destroy]
   get '/microposts', to: 'static_pages#home'
 end
